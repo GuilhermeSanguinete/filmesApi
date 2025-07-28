fce286257e83b7b018266ea5db87e31a5c8c58ff
Guilherme Sanguinete <santos.guilherme.dev@gmail.com>
2025-07-27 21:45:57 -0300

    fix(filmes): adiciona tratamento de erro as funções de controle

--- controllers/filmesController.js
+++ controllers/filmesController.js
@@ -3,55 +3,89 @@ const path = require('path');
 const filepath = path.join(__dirname, '../datas/filmes.json');
 
 function loadFilmes() {
-    return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    try {
+        return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+    }
+    catch{
+        console.error("Erro ao carregar filmes", ex.message)
+    }
 }
 
 function saveFilmes(filmes) {
-    fs.writeFileSync(filepath, JSON.stringify(filmes, null, 2));
+    try {
+        fs.writeFileSync(filepath, JSON.stringify(filmes, null, 2));
+    } catch (ex) {
+        console.error("Erro ao salvar filmes:", ex.message);
+    }
 }
 
 exports.getAllFilmes = (req, res) => {
-    const filmes = loadFilmes();
-    res.json(filmes);
+    try {
+        const filmes = loadFilmes();
+        res.json(filmes);
+    } catch (ex) {
+        console.error("Erro ao consultar filmes:", ex.message);
+    }
 }
 
 exports.getFilmesById = (req, res) => {
-    const filmes = loadFilmes();
-    const filme = filmes.find(a => a.id === parseInt(req.params.id));
-    if (!filme) return res.status(404).json({ message: 'Filme não encontrado' });
-    res.json(filme);
+    try {
+        const filmes = loadFilmes();
+        const filme = filmes.find(a => a.id === parseInt(req.params.id));
+        if (!filme) return res.status(404).json({ message: 'Filme não encontrado' });
+        res.json(filme);
+    }
+    catch (ex) {
+        console.error("Erro ao consultar filme por Id:", ex.message);
+    }
+
 }
 
 exports.createFilme = (req, res) => {
-    const filmes = loadFilmes();
-    const newFilme = {
-        id: Date.now(),
-        nome: req.body.nome,
-        ano: req.body.ano,
-        categoria: req.body.categoria,
-        assistido: req.body.assistido
+    try {
+        const filmes = loadFilmes();
+        const newFilme = {
+            id: Date.now(),
+            nome: req.body.nome,
+            ano: req.body.ano,
+            categoria: req.body.categoria,
+            assistido: req.body.assistido
+        }
+        filmes.push(newFilme);
+        saveFilmes(filmes);
+        res.status(201).json(newFilme);
     }
-    filmes.push(newFilme);
-    saveFilmes(filmes);
-    res.status(201).json(newFilme);
+    catch (ex) {
+        console.error("Erro ao criar filme:", ex.message);
+    }
 }
 
 exports.deleteFilme = (req, res) => {
-  let filmes = loadFilmes();
-  const index = filmes.findIndex(b => b.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: 'Filme não encontrado' });
+    try {
+        let filmes = loadFilmes();
+        const index = filmes.findIndex(b => b.id === parseInt(req.params.id));
+        if (index === -1) return res.status(404).json({ message: 'Filme não encontrado' });
 
-  const deleted = filmes.splice(index, 1);
-  saveFilmes(filmes);
-  res.json(deleted[0]);
+        const deleted = filmes.splice(index, 1);
+        saveFilmes(filmes);
+        res.json(deleted[0]);
+    }
+    catch {
+        console.error("Erro ao deletar filme", ex.message);
+    }
 };
 
 exports.updateFilme = (req, res) => {
-  const filmes = loadFilmes();
-  const index = filmes.findIndex(b => b.id === parseInt(req.params.id));
-  if (index === -1) return res.status(404).json({ message: 'Filme não encontrado' });
+    try {
+        const filmes = loadFilmes();
+        const index = filmes.findIndex(b => b.id === parseInt(req.params.id));
+        if (index === -1) return res.status(404).json({ message: 'Filme não encontrado' });
 
-  filmes[index] = { ...filmes[index], ...req.body };
-  saveFilmes(filmes);
-  res.json(filmes[index]);
+        filmes[index] = { ...filmes[index], ...req.body };
+        saveFilmes(filmes);
+        res.json(filmes[index]);
+    }
+    catch {
+        console.error("Erro ao atualizar filme", ex.message);
+    }
 };
