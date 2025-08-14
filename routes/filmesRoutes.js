57d5153234fbc263f5917929f8f117b9f7f6c1cb
Guilherme Sanguinete <santos.guilherme.dev@gmail.com>
2025-08-14 20:38:02 -0300

    refactor(filmes): organizando rotas seguindo padão de API

--- routes/filmesRoutes.js
+++ routes/filmesRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const filmesController = require('../controllers/filmesController');
 
-router.get('/', filmesController.getAllFilmes);
+router.get('/busca/detail', filmesController.getDetalhesCatalogo);
+
 router.get('/:id', filmesController.getFilmesById);
+
+router.get('/', filmesController.getAllFilmes);
 router.post('/', filmesController.createFilme);
 router.delete('/:id', filmesController.deleteFilme);
 router.put('/:id', filmesController.updateFilme);
-router.get('/busca/detail', filmesController.getDetalhesCatalogo);
 
 module.exports = router;
